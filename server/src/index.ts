99840555fb8aa662519bc3d46ba24c95b87e2437
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-05 16:11:26 +0700

    initial express app

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Response } from 'express'
+
+const app: Express = express()
+const port: number = 4000
+
+app.get('/', (_, res: Response) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
