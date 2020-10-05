4ff78c8580b64eed83826ad0a9fdd4b0865636c2
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-05 21:30:00 +0700

    wireup application

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,21 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import express, { Express, Response } from 'express'
+import express, { Express } from 'express'
+import createGuardianApis from './createGuardianApis'
+import setupGuardianRoutes from './setupGuardianRoutes'
+import createHttpClient from './createHttpClient'
 
 const app: Express = express()
 const port: number = 4000
 
-app.get('/', (_, res: Response) => {
-  res.send('Hello World')
+const { GUARDIAN_KEY, GUARDIAN_URL } = process.env
+
+const guardianClient = createHttpClient(GUARDIAN_URL, {
+  'api-key': GUARDIAN_KEY,
 })
+const guardianApis = createGuardianApis(guardianClient)
+
+setupGuardianRoutes(app, guardianApis)
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
