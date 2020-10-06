8b1484375a35e1f1306d691ecc8bf2ce5cc16c27
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-06 19:33:03 +0700

    refactor

--- server/src/setupGuardianRoutes.ts
+++ server/src/setupGuardianRoutes.ts
@@ -1,21 +1,17 @@
 import { Express, Request, Response } from 'express'
-import {
-  ArticleResponse,
-  ArticlesResponse,
-  GuardianApis,
-} from './createGuardianApis'
+import { GuardianApis } from './createGuardianApis'
 
 export default (app: Express, guardianApis: GuardianApis) => {
   app.get('/articles', async (_, res: Response) => {
-    const articlesResponse: ArticlesResponse = await guardianApis.getArticles()
-    res.send(articlesResponse)
+    const articles = await guardianApis.getArticles()
+    res.status(articles.statusCode)
+    res.send(articles)
   })
 
   app.get('/articles/*', async (req: Request, res: Response) => {
     const id = req.params[0]
-    const articleResponse: ArticleResponse = await guardianApis.getArticleById(
-      id,
-    )
-    res.send(articleResponse)
+    const article = await guardianApis.getArticleById(id)
+    res.status(article.statusCode)
+    res.send(article)
   })
 }
