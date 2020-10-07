04c72ab84146c4ce5a27ea8498667e5146ceda0d
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-07 19:55:40 +0700

    setup layout and initial color mode script

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/core'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/core'
 import { AppProps } from 'next/app'
+import React from 'react'
+import Layout from '../components/layout'
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ColorModeScript />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
