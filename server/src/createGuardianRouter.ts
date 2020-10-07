38e42af328997c33173852a158d7b2e813885542
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-07 10:26:42 +0700

    refactoring and add cors

--- server/src/createGuardianRouter.ts
+++ server/src/createGuardianRouter.ts
@@ -1,17 +1,21 @@
-import { Express, Request, Response } from 'express'
+import { Router, Request, Response } from 'express'
 import { GuardianApis } from './createGuardianApis'
 
-export default (app: Express, guardianApis: GuardianApis) => {
-  app.get('/articles', async (_, res: Response) => {
+export default (guardianApis: GuardianApis) => {
+  const router = Router()
+
+  router.get('/articles', async (_, res: Response) => {
     const articles = await guardianApis.getArticles()
     res.status(articles.statusCode)
     res.send(articles)
   })
 
-  app.get('/articles/*', async (req: Request, res: Response) => {
+  router.get('/articles/*', async (req: Request, res: Response) => {
     const id = req.params[0]
     const article = await guardianApis.getArticleById(id)
     res.status(article.statusCode)
     res.send(article)
   })
+
+  return router
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,23 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express, { Express } from 'express'
+import cors from 'cors'
 import createGuardianApis from './createGuardianApis'
-import setupGuardianRoutes from './setupGuardianRoutes'
+import createGuardianRouter from './createGuardianRouter'
 import createGuardianClient from './createGuardianClient'
 
 const app: Express = express()
 const port: number = 4000
 
+app.use(cors({ origin: 'http://localhost:3000' }))
+
 const { GUARDIAN_KEY, GUARDIAN_URL } = process.env
 
 const guardianClient = createGuardianClient(GUARDIAN_URL, GUARDIAN_KEY)
 const guardianApis = createGuardianApis(guardianClient)
+const guardianRouter = createGuardianRouter(guardianApis)
 
-setupGuardianRoutes(app, guardianApis)
+app.use('/api', guardianRouter)
 
 const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
