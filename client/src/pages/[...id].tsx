53f6685f00070f230eb693e5306b1dd0a3279022
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-08 13:49:44 +0700

    add back button to page

--- client/src/pages/[...id].tsx
+++ client/src/pages/[...id].tsx
@@ -1,12 +1,15 @@
 import { NextPage } from 'next'
 import React from 'react'
 import Error from '../components/error'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import NextLink from 'next/link'
 import {
   ArticleRoot,
   GuardianResponse,
 } from '../repositories/createGuardianApis'
 import { guardianApis } from '../appContext'
 import Detail from '../components/detail'
+import { Button } from '@chakra-ui/core'
 
 interface Props {
   articleResponse: GuardianResponse<ArticleRoot>
@@ -14,16 +17,21 @@ interface Props {
 
 const Page: NextPage<Props> = props => {
   const { articleResponse } = props
-  if (articleResponse.error) {
-    return <Error message={articleResponse.error.message} />
-  }
-
-  if (articleResponse.response) {
-    console.log(articleResponse.response)
-    return <Detail article={articleResponse.response.content} />
-  }
-
-  return null
+  return (
+    <>
+      <NextLink href="/">
+        <Button leftIcon={<ArrowBackIcon />} variant="link" my="3" size="lg">
+          Back
+        </Button>
+      </NextLink>
+      {articleResponse.error && (
+        <Error message={articleResponse.error.message} />
+      )}
+      {articleResponse.response && (
+        <Detail article={articleResponse.response.content} />
+      )}
+    </>
+  )
 }
 
 Page.getInitialProps = async ctx => {
