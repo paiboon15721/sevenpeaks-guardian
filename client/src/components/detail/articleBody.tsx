36d0dbbebb3a376df111e6534346582954ac7c28
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-10-09 14:08:21 +0700

    add body styling

--- client/src/components/detail/articleBody.tsx
+++ client/src/components/detail/articleBody.tsx
@@ -6,7 +6,33 @@ interface Props {
 }
 
 const Comp: React.FC<Props> = ({ body }) => (
-  <Box mt="5" dangerouslySetInnerHTML={{ __html: body }} />
+  <>
+    <Box mt="5" dangerouslySetInnerHTML={{ __html: body }} />
+    <style global jsx>{`
+      .block {
+        border: 1px solid #81e6d9;
+        padding: 18px;
+        margin-bottom: 1rem;
+        border-radius: 1rem;
+      }
+
+      .block-time {
+        margin-bottom: 0.5rem;
+      }
+
+      .block-title {
+        margin-bottom: 0.5rem;
+      }
+
+      img {
+        margin: 1rem 0;
+      }
+
+      ul {
+        margin-left: 2rem;
+      }
+    `}</style>
+  </>
 )
 
 export default Comp
